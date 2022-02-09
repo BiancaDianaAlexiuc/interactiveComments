b4c40d705f7a5bd2703b5b22d94505cfb82cc057
Alexiuc Bianca Diana <bianca.alexiuc@assist.ro>
2022-02-09 14:33:43 +0200

    Added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { collection, getDocs } from "@firebase/firestore";
+import { useState, useEffect } from "react";
+import "./App.scss";
+import { db } from "./firebase-config";
 
-function App() {
+const App = () => {
+  // const setQuote: string[];
+
+  const [quote, setQuote]: [any, any] = useState([]);
+  const quoteCollectionRef = collection(db, "quotes");
+
+  useEffect(() => {
+    const getQuote = async () => {
+      const data = await getDocs(quoteCollectionRef);
+      setQuote(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      //console.log(data);
+    };
+
+    getQuote();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(quote)}
+      {quote.map((qt: any) => {
+        return (
+          <div key={qt.id}>
+            <h1>{qt.author}</h1>
+            <p>{qt.body}</p>
+            {/* {qt.id} */}
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
